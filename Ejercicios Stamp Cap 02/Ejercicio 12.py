5be1d808f3cf95325e41528157eed7e57a748519
leodavid0109 <leodavid0521@gmail.com>
2024-12-01 22:44:53 -0500

    Ejercicio 11 Desarrollado

---
+++ Ejercicios Stamp Cap 02/Ejercicio 12.py
@@ -0,0 +1,10 @@
+def contador_frecuencias(texto_cifrado):
+    pass
+
+
+print("Bienvenido a tu programa de ayuda en la desencriptación de un texto cifrado.")
+print("Por favor, ingresa el texto cifrado que deseas desencriptar:")
+texto_cifrado = input()
+contador_frecuencias(texto_cifrado)
+print("Ingresa la llave que crees que se utilizó para encriptar el texto:")
+llave = input()
